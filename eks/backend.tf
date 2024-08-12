abe9ee2ef9d4e498825c964f9373fe9339b5bf20
Jitendra Pal <77274375+jitendra-40078@users.noreply.github.com>
2024-08-12 21:47:04 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,8 +7,8 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-ews-baket1"
-    region         = "us-east-1"
+    bucket         = "jitendra-s3-bucket"
+    region         = "ap-northeast-2"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
